3b429c57c3f47c3ff4120772bbe571f7d92b3c54
cindy917 <90430653+weihsinyeh@users.noreply.github.com>
2022-12-20 19:55:11 +0800

    Delete Lab10 directory

--- Lab10/interrupt_manager.c
+++
@@ -1,9 +0,0 @@
-#include <xc.h>
-
-void INTERRUPT_Initialize (void)
-{
-    RCONbits.IPEN = 1;      //enable Interrupt Priority mode
-    INTCONbits.GIEH = 1;    //enable high priority interrupt
-    INTCONbits.GIEL = 1;     //disable low priority interrupt
-}
-

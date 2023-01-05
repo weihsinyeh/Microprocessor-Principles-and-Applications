34ff2aa26beab34e3c286d9a2cdcb2c78269c060
cindy917 <90430653+weihsinyeh@users.noreply.github.com>
2023-01-05 15:21:26 +0800

    Update Bonus2.asm

--- Lab6/Bonus2.asm
+++ Lab6/Bonus2.asm
@@ -38,98 +38,103 @@ int:
 ; let pin can receive digital signal 
 MOVLW 0x0f
 MOVWF ADCON1
-CLRF PORTB   ;?port B??
-BSF TRISB, 0 ;?port B???0
-CLRF LATA    ;port A ??0
-BCF TRISA, 0 ;?port A?bit 0 ??output???clear
-BCF TRISA, 1 ;?port A?bit 1 ??output???clear     
-BCF TRISA, 2 ;?port A?bit 2 ??output???clear  
-BCF TRISA, 3 ;?port A?bit 3 ??output???clear
-BCF PORTB, 0 ;????
-CLRF 0x00
+CLRF PORTB   
+BSF TRISB, 0 ;set portB bit 0 is input
+CLRF LATD    ;clear LED in Port D
+;set port D bit 0,1,2,3 to output，and clear
+BCF TRISD, 0 
+BCF TRISD, 1   
+BCF TRISD, 2 
+BCF TRISD, 3 
+BCF PORTB, 0 ;bottom is not clicked
+CLRF 0x00    ;record the the number of click
 ; ckeck button
 check_process:
-   BTFSC PORTB, 0 ;??port B ???0
+   BTFSC PORTB, 0  ;if bottom is pushed skip next line
    BRA check_process
-   test0: MOVLW 0x00
-   CPFSEQ 0x00 
-	GOTO test1
-   GOTO check1
+   test0: 
+	MOVLW 0x00 ;check the number of clicking is 0 (+1)
+	CPFSEQ 0x00 
+	GOTO test1  ;the number of clicking isn't 0 (+1)
+	GOTO check1
    
-   test1: MOVLW 0x01 ;0x01 0x02 0x03
-   CPFSEQ 0x00
-	GOTO test2
-   GOTO check2
+   test1: 
+	MOVLW 0x01 ;check the number of clicking is 1 (+1)
+	CPFSEQ 0x00
+	GOTO test2  ;the number of clicking isn't 1 (+1)
+	GOTO check2
    
-   test2: MOVLW 0x02 ;0x02 0x03
-   CPFSEQ 0x00
-	GOTO test3
-   GOTO check3
+   test2: 
+	MOVLW 0x02 ;check the number of clicking is 2 (+1)
+	CPFSEQ 0x00
+	GOTO test3  ;the number of clicking isn't 2 (+1)
+	GOTO check3
    
-   test3: MOVLW 0x03
-   CPFSEQ 0x00
-	GOTO test0
-   GOTO check4
+   test3: 
+	MOVLW 0x03 ;check the number of clicking is 3 (+1)
+	CPFSEQ 0x00
+	GOTO test0  ;the number of clicking isn't 3 (+1)
+	GOTO check4
 lightup:
     check1: 
-	   BCF PORTB, 0, 0 ;?port B ????????????????
-	   BSF LATA, 0
-	   BSF LATA, 1
-	   BCF LATA, 2
-	   BCF LATA, 3
-	   DELAY d'50', d'180' ;delay 0.25s
-	   MOVLW 0x01
-	   CPFSEQ 0x00
-		GOTO check2 ;??? ;0x00 ;0x02 ; ;0x03
-	   GOTO nolight ;??
+	BCF PORTB, 0, 0 ; clear portB
+	BSF LATD, 0
+	BSF LATD, 1
+	BCF LATD, 2
+	BCF LATD, 3
+	DELAY d'200', d'180' ;delay 0.25s
+	MOVLW 0x01
+	CPFSEQ 0x00
+	    GOTO check2    ;the number of clicking isn't 1 (+1)
+	GOTO nolight 
     check2: 
-	    BCF PORTB, 0, 0 ;?port B ????????????????
-	   BCF LATA, 0
-	   BSF LATA, 1
-	   BSF LATA, 2
-	   BCF LATA, 3
-	   DELAY d'50', d'180' ;delay 0.25s
-	   MOVLW 0x02
-	   CPFSEQ 0x00
-		GOTO check3 ; ???
-	   GOTO nolight ;??
+	BCF PORTB, 0, 0 ; clear portB
+	BCF LATD, 0
+	BSF LATD, 1
+	BSF LATD, 2
+	BCF LATD, 3
+	DELAY d'200', d'180' ;delay 0.25s
+	MOVLW 0x02
+	CPFSEQ 0x00
+	    GOTO check3   ;the number of clicking isn't 2 (+1)
+	GOTO nolight 
 	    
    check3: 
-	    BCF PORTB, 0, 0 ;?port B ????????????????
-	   BCF LATA, 0
-	   BCF LATA, 1
-	   BSF LATA, 2
-	   BSF LATA, 3
-	   DELAY d'50', d'180' ;delay 0.25s
-	   MOVLW 0x03
-	   CPFSEQ 0x00
-		GOTO check4
-	   GOTO nolight
+	BCF PORTB, 0, 0 ; clear portB
+	BCF LATD, 0
+	BCF LATD, 1
+	BSF LATD, 2
+	BSF LATD, 3
+	DELAY d'200', d'180' ;delay 0.25s
+	MOVLW 0x03
+	CPFSEQ 0x00
+	    GOTO check4  ;the number of clicking isn't 3 (+1)
+	GOTO nolight
 	   
   check4: 
-	BCF PORTB, 0, 0 ;?port B ????????????????
-	   BSF LATA, 0
-	   BCF LATA, 1
-	   BCF LATA, 2
-	   BSF LATA, 3
-	   DELAY d'50', d'180' ;delay 0.25s
-	   MOVLW 0x00
-	   CPFSEQ 0x00
-		GOTO check1 ;???
-	   GOTO nolight ;??
+	BCF PORTB, 0, 0 ; clear portB
+	BSF LATD, 0
+	BCF LATD, 1
+	BCF LATD, 2
+	BSF LATD, 3
+	DELAY d'200', d'180' ;delay 0.25s
+	MOVLW 0x00
+	CPFSEQ 0x00
+	    GOTO check1 ;the number of clicking isn't 0 (+1)
+	GOTO nolight 
    nolight:
-	    BCF PORTB, 0, 0 ;?port B ????????????????
-            
-	    CLRF LATA
-	    INCF 0x00
-	    BCF PORTB, 0, 0 ;?port B ????????????????
-	    MOVLW 0x04
-	    CPFSEQ 0x00
-		   GOTO check_process
-	    CLRF 0x00
-	    GOTO check_process
+	BCF PORTB, 0, 0 ; clear portB    
+	CLRF LATD
+	INCF 0x00
+	BCF PORTB, 0, 0 ; clear bottom click for next click event
+	MOVLW 0x04
+	CPFSEQ 0x00
+	   GOTO check_process
+	CLRF 0x00       ; if the number of click is up to four than next will be zero
+	GOTO check_process
+	
     lastlightup:
-	DELAY d'50', d'45' ;delay 0.25s
+	DELAY d'200', d'45' ;delay 0.25s
 	BRA check_process
 end
 

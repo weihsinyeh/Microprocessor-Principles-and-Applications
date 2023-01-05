4d659b2899f14889b9d71c80bd28f0346413c56d
cindy917 <90430653+weihsinyeh@users.noreply.github.com>
2023-01-05 16:25:09 +0800

    Update Advance.asm

--- Lab7/Advance.asm
+++ Lab7/Advance.asm
@@ -86,18 +86,18 @@ Initial:
     
     BSF RCON, IPEN
     BSF INTCON, GIE
-    BCF PIR1, TMR2IF		; to use TIMER2,set TMR2IF、TMR2IE、TMR2IP
+    BCF PIR1, TMR2IF		; to use TIMER2,set TMR2IF?TMR2IE?TMR2IP
     BSF IPR1, TMR2IP
     BSF PIE1 , TMR2IE
-    MOVLW b'11111111'	        ; 將Prescale與Postscale都設為1:16，意思是之後每256個週期才會將TIMER2+1
-    MOVWF T2CON		; 而由於TIMER本身會是以系統時脈/4所得到的時脈為主
-    MOVLW D'122'		; 因此每256 * 4 = 1024個cycles才會將TIMER2 + 1
-    MOVWF PR2			; 若目前時脈為250khz，想要Delay 0.5秒的話，代表每經過125000cycles需要觸發一次Interrupt
-				; 因此PR2應設為 125000 / 1024 = 122.0703125， 約等於122。
+    MOVLW b'11111111'	        ; ?Prescale?Postscale???1:16???????256??????TIMER2+1
+    MOVWF T2CON		; ???TIMER?????????/4????????
+    MOVLW D'122'		; ???256 * 4 = 1024?cycles???TIMER2 + 1
+    MOVWF PR2			; ??????250khz???Delay 0.5?????????125000cycles??????Interrupt
+				; ??PR2??? 125000 / 1024 = 122.0703125? ???122?
     MOVLW D'00100000'
-    MOVWF OSCCON	        ; 記得將系統時脈調整成250kHz
-    CLRF 0x00                  ; 將第幾次run的數量設為0
-    CLRF 0x01                  ; 現在要run 第幾個燈泡
+    MOVWF OSCCON	        ; ??????????250kHz
+    CLRF 0x00                  ; ????run?????0
+    CLRF 0x01                  ; ???run ?????
     MOVLW 0x01
     MOVWF 0x02                  ; need to set;
     MOVLW halfsec
